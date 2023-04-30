8f0cb6ab6e9a799d44cd812588c94c5ffee2b451
hasanali112 <mdhasan.ru.ro@gmail.com>
2023-04-30 11:58:31 +0600

    statictis page add

--- src/Component/Statistic.jsx
+++ src/Component/Statistic.jsx
@@ -1,11 +1,61 @@
 import React from 'react';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 const Statistic = () => {
+    const data = [
+        {
+          name: "Assignment-1",
+         number: 60
+        },
+        {
+          name: "Assignment-2",
+          number: 60
+        },
+        {
+          name: "Assignment-3",
+          number: 59
+        },
+        {
+          name: "Assignment-4",
+          number: 60
+        },
+        {
+          name: "Assignment-5",
+          number: 50
+        },
+        {
+          name: "Assignment-6",
+          number: 55
+        },
+        {
+          name: "Assignment-7",
+          number: 60
+        },
+        {
+          name: "Assignment-8",
+          number:30
+        }
+      ];
     return (
         <div>
-            <h1>this is static page</h1>
+             <AreaChart
+            width={1000}
+            height={500}
+            data={data}
+            margin={{
+                top: 50,
+                right: 30,
+                left: 20,
+                bottom: 0,
+              }}
+            >
+            <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Area type="monotone" dataKey="number" stroke="#8884d8" fill="#8884d8" />
+            </AreaChart>
         </div>
     );
 };
 
-export default Statistic;<h1>this is static page</h1>
+export default Statistic;
